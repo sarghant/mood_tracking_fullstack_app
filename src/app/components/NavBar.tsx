cb7f3fda368c21bf6c178da606173d3016bd636d
sarghant <sghant456@yahoo.com>
2025-06-27 17:51:00 +0400

    Implement auth js authentication with the github provider.

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -2,6 +2,7 @@
 
 import {
   ChevronDown,
+  LogIn,
   LogOut,
   Settings,
   SmileIcon,
@@ -11,9 +12,13 @@ import {
 import Link from "next/link";
 import { useState } from "react";
 import styles from "./NavBar.module.css";
+import { Session } from "next-auth";
+import { signIn, signOut } from "next-auth/react";
 
-const NavBar = () => {
+const NavBar = ({ session }: { session: Session | null }) => {
   const [isExpanded, setIsExpanded] = useState(false);
+  const user = session?.user;
+  console.log(user);
   return (
     <header className="w-full">
       <nav className="w-full flex items-center justify-between gap-2 lg:max-w-[80vw] mx-auto p-6 lg:p-12">
@@ -45,12 +50,21 @@ const NavBar = () => {
             onClick={() => setIsExpanded(!isExpanded)}
           >
             {/* Profile avatar */}
-            <div className="max-w-max">
-              <UserCircle2
-                size={36}
-                strokeWidth={1.5}
-                className="text-slate-700"
-              />
+            <div className="max-w-max rounded-full overflow-hidden">
+              {session != null ? (
+                <img
+                  width={48}
+                  height={48}
+                  src={user?.image!}
+                  alt="User Avatar"
+                />
+              ) : (
+                <UserCircle2
+                  size={36}
+                  strokeWidth={1.5}
+                  className="text-slate-700"
+                />
+              )}
             </div>
             <ChevronDown />
           </button>
@@ -62,40 +76,61 @@ const NavBar = () => {
                 : "rotate-x-90 opacity-0 -z-50"
             } transition duration-200`}
           >
-            <button
-              type="button"
-              className="w-full flex gap-3 lg:gap-4 p-3 rounded-md items-center cursor-pointer group hover:bg-slate-200 focus:bg-slate-200 transition-colors"
-            >
-              <SunIcon
-                size={28}
-                className="text-amber-700/90 group-hover:text-amber-600/70 transition-colors"
-              />
-              <span className="text-[14px] font-medium text-shadow-2xs text-shadow-neutral-400">
-                Current Theme
-              </span>
-            </button>
-            <Link
-              href="/settings"
-              className="w-full flex gap-3 lg:gap-4 p-3 rounded-md items-center group hover:bg-slate-200 focus:bg-slate-200 transition-colors"
-            >
-              <Settings
-                size={28}
-                className="text-cyan-900/90 group-hover:text-cyan-700/70 transition-colors"
-              />
-              <span className="text-[14px] font-medium text-shadow-2xs text-shadow-neutral-400">
-                Account Settings
-              </span>
-            </Link>
-            <Link
-              href="/logout"
-              className="w-full flex gap-3 lg:gap-4 p-3 rounded-md items-center group hover:bg-slate-200 focus:bg-slate-200 transition-colors"
-            >
-              <LogOut
-                size={28}
-                className="text-red-900/60 group-hover:text-red-700/60 transition-colors"
-              />
-              <span>Logout</span>
-            </Link>
+            {session != null ? (
+              <>
+                <button
+                  type="button"
+                  className="w-full flex gap-3 lg:gap-4 p-3 rounded-md items-center cursor-pointer group hover:bg-slate-200 focus:bg-slate-200 transition-colors"
+                >
+                  <SunIcon
+                    size={28}
+                    className="text-amber-700/90 group-hover:text-amber-600/70 group-focus:text-amber-600/70 transition-colors"
+                  />
+                  <span className="text-sm font-medium text-shadow-2xs text-shadow-neutral-400">
+                    Current Theme
+                  </span>
+                </button>
+                <Link
+                  href="/settings"
+                  className="w-full flex gap-3 lg:gap-4 p-3 rounded-md items-center group hover:bg-slate-200 focus:bg-slate-200 transition-colors"
+                >
+                  <Settings
+                    size={28}
+                    className="text-cyan-900/90 group-hover:text-cyan-700/70 group-focus:text-cyan-700/70 transition-colors"
+                  />
+                  <span className="text-sm font-medium text-shadow-2xs text-shadow-neutral-400">
+                    Account Settings
+                  </span>
+                </Link>
+                <button
+                  type="button"
+                  onClick={() => signOut()}
+                  className="cursor-pointer w-full flex gap-3 lg:gap-4 p-3 rounded-md items-center group hover:bg-slate-200 focus:bg-slate-200 transition-colors"
+                >
+                  <LogOut
+                    size={28}
+                    className="text-red-900/60 group-hover:text-red-700/60 group-focus:text-red-700/60 transition-colors"
+                  />
+                  <span className="text-sm font-medium text-shadow-2xs text-shadow-neutral-400">
+                    Logout
+                  </span>
+                </button>
+              </>
+            ) : (
+              <button
+                type="button"
+                onClick={() => signIn("github")}
+                className="cursor-pointer w-full flex gap-3 lg:gap-4 p-3 rounded-md items-center group hover:bg-slate-200 focus:bg-slate-200 transition-colors"
+              >
+                <LogIn
+                  size={28}
+                  className="text-emerald-800/40 group-hover:text-emerald-700/30 group-focus:text-emerald-700/30 transition-colors"
+                />
+                <span className="text-sm font-medium text-shadow-2xs text-shadow-neutral-400">
+                  Login
+                </span>
+              </button>
+            )}
           </div>
         </div>
       </nav>
